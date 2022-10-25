325d5db082741ecdac2a82b89f5a5587f902b292
Isaac Hussain <isaaclearns58@gmail.com>
2022-10-25 09:00:45 +0300

    Minor update

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
-console.log("testing the page");
+"use strict";
+const modalContainer = document.querySelector('#modal-container');
+const optionCPU = document.querySelector('[data-option="cpu"]');
+const optionPlayer = document.querySelector('[data-option="player"]');
+const opponentTag = document.querySelector('[data-tag="opponent"]');
+const markerXBtn = document.querySelector('[data-marker="x"]');
+const markerOBtn = document.querySelector('[data-marker="o"]');
+const playerScore = document.querySelector('[data-score="player"]');
+const opponentScore = document.querySelector('[data-score="opponent"]');
+
+let selectedMarker = null;
+let selectedOpponent = null;
+console.log(playerScore.textContent);
+console.log(opponentScore.textContent);
+
+function selectPlayerMarker(e) {
+    console.log(e);   
+    selectedMarker = 'X';
+    console.log(e.target);
+    e.target.classList.remove("active");
+    e.target.classList.add("active");
+}
+
+
+// function() {
+//     selectedMarker = 'X';
+//     console.log(this);
+//     this.classList.remove("active");
+//     this.classList.add("active");
+// }
+
+markerXBtn.addEventListener('click',selectPlayerMarker());
+
+markerOBtn.addEventListener('click', () => { 
+    selectMarker = 'O';
+    markerXBtn.classList.remove("active");
+    markerOBtn.classList.add("active");
+    console.log(selectMarker);
+});
+
+optionCPU.addEventListener('click', () => {
+    selectedOpponent = 'CPU';
+    opponentTag.textContent = 'memory';
+    modalContainer.style.display = 'none';
+});
+
+optionPlayer.addEventListener('click', () => {
+    selectedOpponent = 'player';
+    opponentTag.textContent = 'person';
+    modalContainer.style.display = 'none';
+});
+
+const createPlayer = () => {
+
+}
